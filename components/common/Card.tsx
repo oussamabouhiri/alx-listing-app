f4310c123409b49c95a8cf3a20e71e3b9f4a3cc6
OUSSAMA BOUHIRI <oussamabouhiri@gmail.com>
2025-09-28 22:22:37 +0100

    Add Card component with title, description, and image

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -1,1 +1,12 @@
+import { CardProps } from '../../interfaces';
+
+export default function Card({ title, description, image }: CardProps) {
+  return (
+    <div className="border rounded-lg shadow p-4">
+      <img src={image} alt={title} className="rounded-md mb-2" />
+      <h2 className="font-bold text-lg">{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
 
