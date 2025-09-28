2b6e1331f2e6defad5f286eb5ae1a5c2841f77ee
OUSSAMA BOUHIRI <oussamabouhiri@gmail.com>
2025-09-28 22:23:05 +0100

    Add Button component with props for label and click

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,1 +1,13 @@
+import { ButtonProps } from '../../interfaces';
+
+export default function Button({ label, onClick }: ButtonProps) {
+  return (
+    <button
+      className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+}
 
